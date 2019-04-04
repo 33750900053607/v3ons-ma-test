71d44fd2fed5421c34089ec85e00d940967ad28a
cloudy <cloudynode@gmail.com>
2019-04-04 11:13:49 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template id="main">
+  <v-ons-navigator swipeable
+    :page-stack="pageStack"
+    @push-page="pageStack.push($event)"
+  ></v-ons-navigator>
+</template>
+
+
+<script>
+import first from './components/First.vue';
+
+export default {
+  data: function() {
+    return {
+      pageStack: [first]
+    }
+  }
+
+}
+</script>
+
+
